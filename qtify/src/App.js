6493b080452554f7ef092ca54a3a8d35daa55f47
sarathtsakd <sarathtsakd@gmail.com>
2026-08-23 19:38:12 +0530

    deployment fix

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
 import Hero from './Components/Hero/Hero';
 
